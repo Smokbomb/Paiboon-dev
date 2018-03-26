b437be22fb400b52aa331c6695cd42be648d5288
DESKTOP-JF5L88J\VerBack-TobTab <paiboon@vertice-international.com>
2018-03-26 17:19:33 +0700

    update

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
-
+  public angRating = [5, 4, true];
+  public nodeRating = [5, 3, true];
+  public phpRating = [5, 5, true];
+  public rjavaRating = [5, 3, true];
+  public rjavaScriptRating = [5, 4, true];
   constructor() { }
 
   ngOnInit() {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
-
+import { RatingModule } from 'ngx-bootstrap/rating';
+import { AccordionModule } from 'ngx-bootstrap';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -18,6 +20,9 @@ import { AboutComponent } from './about/about.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    RatingModule.forRoot(),
+    AccordionModule.forRoot(),
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
